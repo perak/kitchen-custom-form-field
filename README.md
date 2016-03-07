0ecde917a2bbab30717613eb6abe2926561ad325
Petar Korponaić <petar.korponaic@gmail.com>
2016-03-07 22:01:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Custom Form Field
+=================
+
+This example shows how to add custom field into meteor-kitchen's built-in form component. It's actually a hack but it works. Better handling for custom form fields will be added in (near) future...
+
+Covers issues:
+
+https://github.com/perak/kitchen-cli/issues/13
+https://github.com/perak/kitchen-site/issues/260
+https://github.com/perak/kitchen-site/issues/257
+
+Enjoy! :)
---
+++ app/client/views/customers/details/details_controller.js
@@ -0,0 +1,49 @@
+this.CustomersDetailsController = RouteController.extend({
+	template: "CustomersDetails",
+	
+
+	yieldTemplates: {
+		/*YIELD_TEMPLATES*/
+	},
+
+	onBeforeAction: function() {
+		this.next();
+	},
+
+	action: function() {
+		if(this.isReady()) { this.render(); } else { this.render("loading"); }
+		/*ACTION_FUNCTION*/
+	},
+
+	isReady: function() {
+		
+
+		var subs = [
+			Meteor.subscribe("customer", this.params.customerId)
+		];
+		var ready = true;
+		_.each(subs, function(sub) {
+			if(!sub.ready())
+				ready = false;
+		});
+		return ready;
+	},
+
+	data: function() {
+		
+
+		var data = {
+			params: this.params || {},
+			customer: Customers.findOne({_id:this.params.customerId}, {})
+		};
+		
+
+		
+
+		return data;
+	},
+
+	onAfterAction: function() {
+		
+	}
+});
